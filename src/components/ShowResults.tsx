9f0f377d55b321c0366735e2711b473736f62818
mca36 <89984970+marcus-24@users.noreply.github.com>
2024-10-18 18:20:03 -0400

    Added weather tabs as flex container and css hover animations

--- src/components/ShowResults.tsx
+++ src/components/ShowResults.tsx
@@ -3,17 +3,56 @@ import axios from "axios";
 import { ReactElement } from "react";
 import { IWeather, ResultProps } from "../interfaces";
 
+const DEFAULTDATEOPTIONS = { month: "long", day: "numeric", year: "numeric" };
+
 async function retrieveWeather(url: string): Promise<IWeather[]> {
   const res = await axios.get(url);
   return res.data;
 }
 
-function WeatherBox(props: { name: string }) {
+function displayFriendlyDate(
+  date: string,
+  options: any = DEFAULTDATEOPTIONS
+): string {
+  const dateObj = new Date(date);
+  return dateObj.toLocaleDateString("en-US", options);
+}
+
+function WeatherFigure({ val }: { val: IWeather }) {
   return (
-    <div className="weather-item">
-      <p>{props.name}</p>
-    </div>
+    <figure className="img-container">
+      <img src={val.icon} alt={`Forecast for ${val.name}`} />
+      <figcaption>
+        <p>{val.name}</p>
+        <p>
+          {val.temperature}°{val.temperatureUnit}
+        </p>
+        <p>{val.shortForecast}</p>
+      </figcaption>
+    </figure>
   );
+}
+
+function WeatherItem({ weatherDay }: { weatherDay: IWeather[] }): ReactElement {
+  const date: string = weatherDay[0].startTime.split("T")[0];
+  return (
+    <>
+      <h1>{displayFriendlyDate(date)}</h1>
+      <div className="weather-item">
+        {weatherDay.map((val: IWeather) => (
+          <WeatherFigure val={val} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+function get_unique_dates(weather: IWeather[]): string[] {
+  const start_dates: string[] = Array.from(
+    weather,
+    (item) => item.startTime.split("T")[0]
+  ); //extract start times from IWeather objects
+  return Array.from(new Set(start_dates)); // extract unique dates
 }
 
 export function ShowResults({ city, baseUrl }: ResultProps): ReactElement {
@@ -31,13 +70,21 @@ export function ShowResults({ city, baseUrl }: ResultProps): ReactElement {
   if (isLoading) return <h1>Finding matching cities....</h1>;
   else if (isError)
     return <h1>{`Could not find weather for ${city}. Please try again.`}</h1>;
-  else {
+  else if (city) {
+    const unique_start_dates: string[] = get_unique_dates(weatherList!);
     return (
       <div className="container" id="column">
-        {weatherList!.map((weather: IWeather) => (
-          <WeatherBox name={weather.name}></WeatherBox>
-        ))}
+        {unique_start_dates.map((date: string) => {
+          const weatherDay: IWeather[] = weatherList!.filter(
+            (item: IWeather) => {
+              return item.startTime.includes(date);
+            }
+          );
+          return <WeatherItem weatherDay={weatherDay} />;
+        })}
       </div>
     );
+  } else {
+    return <></>;
   }
 }
