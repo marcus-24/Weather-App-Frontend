8288af11529327ab996bf7fab2a208e280972344
mca36 <89984970+marcus-24@users.noreply.github.com>
2024-11-02 14:03:37 -0400

    Added simulated actions in unit tests

--- src/__tests__/components/ShowResults.test.tsx
+++ src/__tests__/components/ShowResults.test.tsx
@@ -32,7 +32,20 @@ describe("Test Weather Figure Component", () => {
   });
 
   test("Sanity check for first ReactJS test", () => {
-    const imgElement = screen.getByRole("figure");
+    const imgElement = screen.getByRole("img");
     expect(imgElement).toBeInTheDocument();
+  });
+
+  test("Check image tag", () => {
+    const imgElement = screen.getByRole("img");
+    expect(imgElement).toHaveAttribute("src", "myfakeurl");
+    expect(imgElement).toHaveAttribute("alt", "Forecast for Tuesday");
+  });
+
+  test("Check image caption", () => {
+    const figElement = screen.getByRole("figure");
+    expect(figElement).toHaveTextContent("Tuesday");
+    expect(figElement).toHaveTextContent("0°F");
+    expect(figElement).toHaveTextContent("Rainy and Warm");
   });
 });
